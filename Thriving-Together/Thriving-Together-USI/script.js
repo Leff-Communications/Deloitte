1609a9405437678dacf4aedda60e93c2aaa0649a
Juan Sosa <juan.sosa@leffcommunications.com>
2023-08-28 17:48:32 -0500

    updated clickables

--- Thriving-Together/Thriving-Together-USI/script.js
+++ Thriving-Together/Thriving-Together-USI/script.js
@@ -1,165 +1,52 @@
-const modal1 = document.querySelector('#modal-1');
-const modal2 = document.querySelector('#modal-2');
-const modal3 = document.querySelector('#modal-3');
-const modal4 = document.querySelector('#modal-4');
-const modal5 = document.querySelector('#modal-5');
-
-
 const close1 = document.querySelector('#close-1');
 const close2 = document.querySelector('#close-2');
 const close3 = document.querySelector('#close-3');
-const close4 = document.querySelector('#close-4');;
-const close5 = document.querySelector('#close-5');
+const close4 = document.querySelector('#close-4');
 
 
-const openModal1 = document.querySelector('#tile-1-hover');
-const openModal2 = document.querySelector('#tile-2-hover');
-const openModal3 = document.querySelector('#tile-3-hover');
-const openModal4 = document.querySelector('#tile-4-hover');
-const openModal5 = document.querySelector('#tile-5-hover');
+const open1 = document.querySelector('#tile-1');
+const open2 = document.querySelector('#tile-2');
+const open3 = document.querySelector('#tile-3');
+const open4 = document.querySelector('#tile-4');
+
+const active1 = document.querySelector('#tile-1-active');
+const active2 = document.querySelector('#tile-2-active');
+const active3 = document.querySelector('#tile-3-active');
+const active4 = document.querySelector('#tile-4-active');
 
 
-openModal1.addEventListener('click', function (event) {
-    modal1.classList.add("modal-open");
+open1.addEventListener('click', function (event) {
+    console.log("clicked");
+    active1.classList.add("active");
 })
 
-openModal2.addEventListener('click', function (event) {
-    modal2.classList.add("modal-open");
-})
-
-openModal3.addEventListener('click', function (event) {
-    modal3.classList.add("modal-open");
-})
-
-openModal4.addEventListener('click', function (event) {
-    modal4.classList.add("modal-open");
-})
-
-openModal5.addEventListener('click', function (event) {
-    modal5.classList.add("modal-open");
-})
-
-//the below functions are used to calculate the size of the modal div that opens. 
-//Clicking on any grey space outside of the modal would end up closing the modal 
-//and allowing the user to see the beginning screen
-modal1.addEventListener('click', function (event) {
-    var rect = modal1.children[0].getBoundingClientRect();
-        var isInDialog=(rect.top <= event.clientY && event.clientY <= rect.top + rect.height
-          && rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
-        if (!isInDialog) {
-            console.log("out")
-            modal1.classList.remove("modal-open");
-        }
-})
-
-modal2.addEventListener('click', function (event) {
-    var rect = modal2.children[0].getBoundingClientRect();
-        var isInDialog=(rect.top <= event.clientY && event.clientY <= rect.top + rect.height
-          && rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
-        if (!isInDialog) {
-            modal2.classList.remove("modal-open");
-        }
-})
-
-modal3.addEventListener('click', function (event) {
-    var rect = modal3.children[0].getBoundingClientRect();
-        var isInDialog=(rect.top <= event.clientY && event.clientY <= rect.top + rect.height
-          && rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
-        if (!isInDialog) {
-            modal3.classList.remove("modal-open");
-        }
-})
-
-modal4.addEventListener('click', function (event) {
-    var rect = modal4.children[0].getBoundingClientRect();
-        var isInDialog=(rect.top <= event.clientY && event.clientY <= rect.top + rect.height
-          && rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
-        if (!isInDialog) {
-            modal4.classList.remove("modal-open");
-        }
-})
-
-modal5.addEventListener('click', function (event) {
-    var rect = modal5.children[0].getBoundingClientRect();
-        var isInDialog=(rect.top <= event.clientY && event.clientY <= rect.top + rect.height
-          && rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
-        if (!isInDialog) {
-            modal5.classList.remove("modal-open");
-        }
-})
-
-//all functions below are used to close any modal after hitting the 'X' button on a modal
-close1.addEventListener('click', () => {
-    modal1.classList.remove("modal-open");
-    modal2.classList.remove("modal-open");
-    modal3.classList.remove("modal-open");
-    modal4.classList.remove("modal-open");
-    modal5.classList.remove("modal-open");
-})
-
-close2.addEventListener('click', () => {
-    modal1.classList.remove("modal-open");
-    modal2.classList.remove("modal-open");
-    modal3.classList.remove("modal-open");
-    modal4.classList.remove("modal-open");
-    modal5.classList.remove("modal-open");
-})
-
-close3.addEventListener('click', () => {
-    modal1.classList.remove("modal-open");
-    modal2.classList.remove("modal-open");
-    modal3.classList.remove("modal-open");
-    modal4.classList.remove("modal-open");
-    modal5.classList.remove("modal-open");
-})
-
-close4.addEventListener('click', () => {
-    modal1.classList.remove("modal-open");
-    modal2.classList.remove("modal-open");
-    modal3.classList.remove("modal-open");
-    modal4.classList.remove("modal-open");
-    modal5.classList.remove("modal-open");
-})
-
-close5.addEventListener('click', () => {
-    modal1.classList.remove("modal-open");
-    modal2.classList.remove("modal-open");
-    modal3.classList.remove("modal-open");
-    modal4.classList.remove("modal-open");
-    modal5.classList.remove("modal-open");
-})
-
-//each function below opens the respective modal using the navigation at the bottom of each modal
-//(nav1() is used for opening the modal associated with tile-1; nav(2) is used for tile-2; etc.)
-
-//Each is called with the onclick attribute within the html
-function nav1() {
-    modal5.classList.remove("modal-open");
-    modal2.classList.remove("modal-open");
-    modal1.classList.add("modal-open");
+function openTile1() {
+    console.log("clicked");
+    active1.classList.add("active");
 }
 
-function nav2() {
-    modal1.classList.remove("modal-open");
-    modal3.classList.remove("modal-open");
-    modal2.classList.add("modal-open");
-}
+open2.addEventListener('click', function (event) {
+    active2.classList.add("active");
+})
 
-function nav3() {
-    modal2.classList.remove("modal-open");
-    modal4.classList.remove("modal-open");
-    modal3.classList.add("modal-open");
-}
+open3.addEventListener('click', function (event) {
+    active3.classList.add("active");
+})
 
-function nav4() {
-    modal3.classList.remove("modal-open");
-    modal5.classList.remove("modal-open");
-    modal4.classList.add("modal-open");
-}
+open4.addEventListener('click', function (event) {
+    active4.classList.add("active");
+})
 
-function nav5() {
-    console.log('clicked');
-    modal4.classList.remove("modal-open");
-    modal1.classList.remove("modal-open");
-    modal5.classList.add("modal-open");
-}
+close1.addEventListener('click', function (event) {
+    active1.classList.remove("active");
+})
+
+close2.addEventListener('click', function (event) {
+    active2.classList.remove("active");
+})
+close3.addEventListener('click', function (event) {
+    active3.classList.remove("active");
+})
+close4.addEventListener('click', function (event) {
+    active4.classList.remove("active");
+})
